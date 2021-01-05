03639396c0c5364c27827cfc27b507e5bc768120
Alexiosdut <Alexiosdut@gmail.com>
2021-01-05 21:55:25 +0200

    fix navbar button and swap to mongodb atlas

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const expressValidator = require("express-validator");
-const methodOverride = require("method-override");
-const fileUpload = require("express-fileupload");
-const passport = require("passport");
-const moment = require("moment");
-const cookieParser = require("cookie-parser");
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const expressValidator = require('express-validator')
+const methodOverride = require('method-override')
+const fileUpload = require('express-fileupload')
+const passport = require('passport')
+const moment = require('moment')
+const cookieParser = require('cookie-parser')
 
-const { database } = require("./config/database");
+const { database } = require('./config/database')
 
 // init app
-const app = express();
+const app = express()
 
 // connect to mongo
-mongoose.connect(database, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("Connected to Mongo");
-});
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('Connected to Mongo')
+})
 
 //  view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // set public folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // express fileupload middleware
-app.use(fileUpload());
+app.use(fileUpload())
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // 1
-app.use(cookieParser());
+app.use(cookieParser())
 
 // 2
 // app.use(session({ secret: 'krunal', saveUninitialized: true, resave: false }));
@@ -46,157 +46,157 @@ app.use(cookieParser());
 // express session middleware
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
     // cookie: { secure: true }
-  })
-);
+  }),
+)
 
 // express validator middleware
 app.use(
   expressValidator({
     customValidators: {
-      isImage: function(value, filename) {
-        let extension = path.extname(filename).toLowerCase();
+      isImage: function (value, filename) {
+        let extension = path.extname(filename).toLowerCase()
 
         // return extension == '.jpg'
         switch (extension) {
-          case ".jpg":
-            return ".jpg";
-          case ".jpeg":
-            return ".jpeg";
-          case ".png":
-            return ".png";
+          case '.jpg':
+            return '.jpg'
+          case '.jpeg':
+            return '.jpeg'
+          case '.png':
+            return '.png'
           // case '':
           //   return '.jpg';
           default:
-            return false;
+            return false
         }
-      }
-    }
-  })
-);
+      },
+    },
+  }),
+)
 
 // express messages middleware
-app.use(require("connect-flash")());
-app.use(function(req, res, next) {
-  res.locals.messages = require("express-messages")(req, res);
-  next();
-});
+app.use(require('connect-flash')())
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 // passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport)
 
 // passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // method-override middleware - override with POST having ?_method=PUT
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'))
 
 // local variables - ta local variables xononte san parametroi sta views
-app.use("/", (req, res, next) => {
-  app.locals.errors = null;
-  app.locals.title = null;
-  app.locals.slug = null;
-  app.locals.content = null;
-  app.locals.body = null;
-  app.locals.noMatch = null;
-  app.locals.display = false;
+app.use('/', (req, res, next) => {
+  app.locals.errors = null
+  app.locals.title = null
+  app.locals.slug = null
+  app.locals.content = null
+  app.locals.body = null
+  app.locals.noMatch = null
+  app.locals.display = false
   // app.locals.errorImg = req.session.errors;
 
-  app.locals.generateDate = function(date, format) {
-    return moment(date).format(format);
-  };
+  app.locals.generateDate = function (date, format) {
+    return moment(date).format(format)
+  }
 
-  next();
-});
+  next()
+})
 
 // get page model
-let Page = require("./models/page");
+let Page = require('./models/page')
 
 // get all pages to pass to header.ejs
-app.use("/", (req, res, next) => {
+app.use('/', (req, res, next) => {
   Page.find({})
     .sort({ sorting: 1 })
     .exec()
     .then(pages => {
-      app.locals.pages = pages;
+      app.locals.pages = pages
 
-      next();
-    });
-});
+      next()
+    })
+})
 
 // get category model
-let Category = require("./models/category");
+let Category = require('./models/category')
 
 // get all categories to pass to header.ejs
-app.use("/", (req, res, next) => {
+app.use('/', (req, res, next) => {
   Category.find({}).then(categories => {
-    app.locals.categories = categories;
+    app.locals.categories = categories
 
-    next();
-  });
-});
+    next()
+  })
+})
 
 // local variables
-app.get("*", (req, res, next) => {
-  res.locals.cart = req.session.cart;
+app.get('*', (req, res, next) => {
+  res.locals.cart = req.session.cart
 
-  res.locals.user = req.user || null; // ean o xristis kanei log in tha exoume prosvash sto req.user, allios tha einai null
+  res.locals.user = req.user || null // ean o xristis kanei log in tha exoume prosvash sto req.user, allios tha einai null
 
-  res.locals.pAvailability = "YES";
+  res.locals.pAvailability = 'YES'
 
   // gia to search sti kathe katigoria proionton
-  res.locals.currentCat = req.query.currentCat;
-  res.locals.cat = null;
-  res.locals.cate = null;
+  res.locals.currentCat = req.query.currentCat
+  res.locals.cat = null
+  res.locals.cate = null
 
-  next();
-});
+  next()
+})
 
 // set routes
-const home = require("./routes/home.js");
-const pages = require("./routes/pages.js");
-const adminPages = require("./routes/admin-pages.js");
-const adminCategories = require("./routes/admin-categories.js");
-const adminProducts = require("./routes/admin-products.js");
-const adminBlogPosts = require("./routes/admin-blog-posts.js");
-const adminCostumOrders = require("./routes/admin-costum-orders");
-const adminNewsLetter = require("./routes/admin-news-letter");
-const adminFeedback = require("./routes/admin-feedback.js");
-const products = require("./routes/products.js");
-const blog = require("./routes/blog.js");
-const cart = require("./routes/cart.js");
-const users = require("./routes/users.js");
-const contact = require("./routes/contact.js");
-const feedback = require("./routes/feedback.js");
-const popularProducts = require("./routes/popular-products");
-const sortByPrice = require("./routes/sortByPrice");
+const home = require('./routes/home.js')
+const pages = require('./routes/pages.js')
+const adminPages = require('./routes/admin-pages.js')
+const adminCategories = require('./routes/admin-categories.js')
+const adminProducts = require('./routes/admin-products.js')
+const adminBlogPosts = require('./routes/admin-blog-posts.js')
+const adminCostumOrders = require('./routes/admin-costum-orders')
+const adminNewsLetter = require('./routes/admin-news-letter')
+const adminFeedback = require('./routes/admin-feedback.js')
+const products = require('./routes/products.js')
+const blog = require('./routes/blog.js')
+const cart = require('./routes/cart.js')
+const users = require('./routes/users.js')
+const contact = require('./routes/contact.js')
+const feedback = require('./routes/feedback.js')
+const popularProducts = require('./routes/popular-products')
+const sortByPrice = require('./routes/sortByPrice')
 
 // load routes
 // comment
-app.use("/", home);
-app.use("/nav", pages);
-app.use("/admin/pages", adminPages);
-app.use("/admin/categories", adminCategories);
-app.use("/admin/products", adminProducts);
-app.use("/admin/blog-posts", adminBlogPosts);
-app.use("/admin/costum-orders", adminCostumOrders);
-app.use("/admin/news-letter", adminNewsLetter);
-app.use("/admin/feedback", adminFeedback);
-app.use("/products", products);
-app.use("/blog", blog);
-app.use("/cart", cart);
-app.use("/users", users);
-app.use("/contact", contact);
-app.use("/feedback", feedback);
-app.use("/popular-products", popularProducts);
-app.use("/sort", sortByPrice);
+app.use('/', home)
+app.use('/nav', pages)
+app.use('/admin/pages', adminPages)
+app.use('/admin/categories', adminCategories)
+app.use('/admin/products', adminProducts)
+app.use('/admin/blog-posts', adminBlogPosts)
+app.use('/admin/costum-orders', adminCostumOrders)
+app.use('/admin/news-letter', adminNewsLetter)
+app.use('/admin/feedback', adminFeedback)
+app.use('/products', products)
+app.use('/blog', blog)
+app.use('/cart', cart)
+app.use('/users', users)
+app.use('/contact', contact)
+app.use('/feedback', feedback)
+app.use('/popular-products', popularProducts)
+app.use('/sort', sortByPrice)
 
 // start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+  console.log(`Server started on port ${port}`)
+})
--- config/database.js
+++ config/database.js
@@ -4,6 +4,9 @@ module.exports = {
   // database: 'mongodb://cms-user:123abcz@ds215822.mlab.com:15822/cms'
 
   // new database
+  // database:
+  //   "mongodb://ptixiakh-user:ptixiakh123a@ds151383.mlab.com:51383/ptixiakh"
+
   database:
-    "mongodb://ptixiakh-user:ptixiakh123a@ds151383.mlab.com:51383/ptixiakh"
-};
+    'mongodb+srv://Alex123:alex123@cluster0.bhwtm.mongodb.net/3dlads?retryWrites=true&w=majority',
+}
